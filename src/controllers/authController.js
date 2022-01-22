e319881852e403193357d149a0f0d5f874710520
vininribeiro23 <vininribeiro@hotmail.com>
2022-01-22 01:46:44 -0300

    autenticação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
 const  express = require ('express')
+const bcrypt = require ('bcryptjs')
+const jwt =  require ('jsonwebtoken')
 
 const User = require('../models/User')
 
@@ -20,4 +22,20 @@ try {
 }
 });
 
+router.post('/authenticate', async(req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+    if(!user) 
+    return res.status(400).send({ error: 'User not found'})
+
+    if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Invalid password'});
+
+    user.password = undefined;
+     
+    const token = jwt.sign({ id: user.id}, )
+
+    res.send({ user});
+});
 module.exports = app => app.use('/auth', router);
